aadca02efbf2b35da38b1ae06fe0b84e75085d9b
Ronja-Burger <ronja.burger@hs-furtwangen.de>
2022-03-25 19:54:11 +0100

    Aufgabe 1 fast fertig

--- Zufallsgedicht/RandomPoemScript.js
+++ Zufallsgedicht/RandomPoemScript.js
@@ -4,30 +4,37 @@ var RandomPoem;
     let subjekte = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let prädikate = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objekte = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
-    console.log(subjekte, prädikate, objekte);
+    //console.log (subjekte, prädikate, objekte);
     for (let index = 5; index >= 1; index--) {
         let solution = getVerse(subjekte, prädikate, objekte);
         //console.log(index);
         //console.log(solution);
     }
     function getVerse(_subjekte, _prädikate, _objekte) {
-        let einVers = [""];
-        //let test: string = "Alohomora";
-        return einVers;
-        //console.log(test);
+        //creation of numbers: SUBJECT
+        let zahlSub = Math.floor(Math.random() * 6);
+        let subZahl = zahlSub;
+        //console.log(subZahl);
+        // Zufallswert aus den Subjekten
+        let removedSubject = subjekte.splice(subZahl, 1);
+        //console.log(removedSubject + " ");
+        //creation of numbers: PRÄDIKAT
+        let zahlPrä = Math.floor(Math.random() * 6);
+        let präZahl = zahlPrä;
+        //console.log(präZahl);
+        // Zufallswert aus den Prädikaten
+        let removedPrädikat = prädikate.splice(präZahl, 1);
+        //console.log(removedPrädikat + " ");
+        //creation of numbers: OBJEKT
+        let zahlObj = Math.floor(Math.random() * 6);
+        let objZahl = zahlObj;
+        //console.log(objZahl);
+        // Zufallswert aus den Objekten
+        let removedObject = objekte.splice(objZahl, 1);
+        //console.log(removedObject + " ");
+        let einVers = [removedSubject[], removedPrädikat[], removedObject[]];
+        console.log(einVers);
+        return;
     }
-    //creation of numbers
-    let eineZahl = Math.floor(Math.random() * 6);
-    let dieZahl = eineZahl;
-    console.log(dieZahl);
-    // Zufallswert aus den Subjekten
-    let removedSubject = subjekte.splice(dieZahl, 1);
-    console.log(removedSubject + " ");
-    // Zufallswert aus den Prädikaten
-    let removedPrädikat = prädikate.splice(dieZahl, 1);
-    console.log(removedPrädikat + " ");
-    // Zufallswert aus den Objekten
-    let removedObject = objekte.splice(dieZahl, 1);
-    console.log(removedObject + " ");
 })(RandomPoem || (RandomPoem = {}));
 //# sourceMappingURL=RandomPoemScript.js.map
