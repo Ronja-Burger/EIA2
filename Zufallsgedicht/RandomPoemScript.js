df6860640f132e18faec4c5d91152c7c513aa374
Ronja-Burger <ronja.burger@hs-furtwangen.de>
2022-03-25 18:53:01 +0100

    Repository erneuert

--- Zufallsgedicht/RandomPoemScript.js
+++ Zufallsgedicht/RandomPoemScript.js
@@ -1,4 +1,33 @@
 "use strict";
-let beispiel = "RandomPoem";
-console.log(beispiel);
+var RandomPoem;
+(function (RandomPoem) {
+    let subjekte = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let prädikate = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objekte = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    console.log(subjekte, prädikate, objekte);
+    for (let index = 5; index >= 1; index--) {
+        let solution = getVerse(subjekte, prädikate, objekte);
+        //console.log(index);
+        //console.log(solution);
+    }
+    function getVerse(_subjekte, _prädikate, _objekte) {
+        let einVers = [""];
+        //let test: string = "Alohomora";
+        return einVers;
+        //console.log(test);
+    }
+    //creation of numbers
+    let eineZahl = Math.floor(Math.random() * 6);
+    let dieZahl = eineZahl;
+    console.log(dieZahl);
+    // Zufallswert aus den Subjekten
+    let removedSubject = subjekte.splice(dieZahl, 1);
+    console.log(removedSubject + " ");
+    // Zufallswert aus den Prädikaten
+    let removedPrädikat = prädikate.splice(dieZahl, 1);
+    console.log(removedPrädikat + " ");
+    // Zufallswert aus den Objekten
+    let removedObject = objekte.splice(dieZahl, 1);
+    console.log(removedObject + " ");
+})(RandomPoem || (RandomPoem = {}));
 //# sourceMappingURL=RandomPoemScript.js.map
--- Zufallsgedicht/RandomPoemScript.ts
+++ Zufallsgedicht/RandomPoemScript.ts
@@ -1,2 +1,36 @@
-let beispiel: string = "RandomPoem";
-console.log(beispiel);
+namespace RandomPoem {
+    let subjekte: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let prädikate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"] ;
+    let objekte: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    console.log (subjekte, prädikate, objekte);
+    for (let index: number = 5; index >= 1; index--) {
+        let solution: string = getVerse(subjekte, prädikate, objekte);
+        //console.log(index);
+        //console.log(solution);
+    }
+
+    function getVerse(_subjekte: string[], _prädikate: string[], _objekte: string[]): string {
+        let einVers: string[] = [""];
+        //let test: string = "Alohomora";
+        return einVers;
+        //console.log(test);
+    }
+
+    //creation of numbers
+
+    let eineZahl: number = Math.floor(Math.random() * 6 );
+    let dieZahl: number = eineZahl;
+    console.log(dieZahl);
+
+    // Zufallswert aus den Subjekten
+    let removedSubject: string = subjekte.splice(dieZahl, 1);
+    console.log(removedSubject + " ");
+
+     // Zufallswert aus den Prädikaten
+     let removedPrädikat: string = prädikate.splice(dieZahl, 1);
+     console.log(removedPrädikat + " ");
+
+      // Zufallswert aus den Objekten
+    let removedObject: string = objekte.splice(dieZahl, 1);
+    console.log(removedObject + " ");
+    }
