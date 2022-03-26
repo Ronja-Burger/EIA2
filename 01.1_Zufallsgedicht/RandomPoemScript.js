ba5b76f6e2b110675e8098165d498cf60c20b94c
Ronja-Burger <ronja.burger@hs-furtwangen.de>
2022-03-26 15:09:41 +0100

    Zufallsgedicht FERTIG XD

--- 01.1_Zufallsgedicht/RandomPoemScript.js
+++ 01.1_Zufallsgedicht/RandomPoemScript.js
@@ -2,40 +2,35 @@
 var RandomPoem;
 (function (RandomPoem) {
     let subjekte = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-    let prädikate = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let praedikate = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objekte = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
     //console.log (subjekte, prädikate, objekte);
     for (let index = 5; index >= 1; index--) {
-        let solution = getVerse(subjekte, prädikate, objekte);
+        let solution = getVerse(subjekte, praedikate, objekte);
         //console.log(index);
-        //console.log(solution);
+        console.log(solution);
     }
     function getVerse(_subjekte, _prädikate, _objekte) {
         //creation of numbers: SUBJECT
-        let zahlSub = Math.floor(Math.random() * 6);
-        let subZahl = zahlSub;
+        let zahlSub = Math.floor(Math.random() * _subjekte.length);
         //console.log(subZahl);
         // Zufallswert aus den Subjekten
-        let removedSubject = subjekte.splice(subZahl, 1);
+        let removedSubject = subjekte.splice(zahlSub, 1);
         //console.log(removedSubject + " ");
         //creation of numbers: PRÄDIKAT
-        let zahlPrä = Math.floor(Math.random() * 6);
-        let präZahl = zahlPrä;
+        let zahlPra = Math.floor(Math.random() * _prädikate.length);
         //console.log(präZahl);
         // Zufallswert aus den Prädikaten
-        let removedPraedikat = prädikate.splice(präZahl, 1);
+        let removedPraedikat = praedikate.splice(zahlPra, 1);
         //console.log(removedPrädikat + " ");
         //creation of numbers: OBJEKT
-        let zahlObj = Math.floor(Math.random() * 6);
-        let objZahl = zahlObj;
+        let zahlObj = Math.floor(Math.random() * _objekte.length);
         //console.log(objZahl);
         // Zufallswert aus den Objekten
-        let removedObject = objekte.splice(objZahl, 1);
+        let removedObject = objekte.splice(zahlObj, 1);
         //console.log(removedObject + " ");
-        let einVers = removedSubject[0], removedPraedikat, [];
-        0;
-        removedObject[0];
-        console.log(einVers);
+        let einVers = removedSubject[0] + " " + removedPraedikat[0] + " " + removedObject[0];
+        //console.log(einVers);
         return einVers;
     }
 })(RandomPoem || (RandomPoem = {}));
--- 01.1_Zufallsgedicht/RandomPoemScript.ts
+++ 01.1_Zufallsgedicht/RandomPoemScript.ts
@@ -1,30 +1,29 @@
 namespace RandomPoem {
     let subjekte: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-    let prädikate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"] ;
+    let praedikate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let objekte: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
     //console.log (subjekte, prädikate, objekte);
 
 
 
     for (let index: number = 5; index >= 1; index--) {
-        let solution: string = getVerse(subjekte, prädikate, objekte);
+        let solution: string = getVerse(subjekte, praedikate, objekte);
         //console.log(index);
-        //console.log(solution);
+        console.log(solution);
     }
 
 
 
 
     function getVerse(_subjekte: string[], _prädikate: string[], _objekte: string[]): string {
-    
+
         //creation of numbers: SUBJECT
-    let zahlSub: number = Math.floor(Math.random() * 6 );
-    let subZahl: number = zahlSub;
-    //console.log(subZahl);
+        let zahlSub: number = Math.floor(Math.random() * _subjekte.length);
+        //console.log(subZahl);
 
         // Zufallswert aus den Subjekten
-    let removedSubject: string = subjekte.splice(subZahl, 1);
-    //console.log(removedSubject + " ");
+        let removedSubject: string[] = subjekte.splice(zahlSub, 1);
+        //console.log(removedSubject + " ");
 
 
 
@@ -32,13 +31,12 @@ namespace RandomPoem {
 
 
         //creation of numbers: PRÄDIKAT
-    let zahlPrä: number = Math.floor(Math.random() * 6 );
-    let präZahl: number = zahlPrä;
-    //console.log(präZahl);
+        let zahlPra: number = Math.floor(Math.random() * _prädikate.length);
+        //console.log(präZahl);
 
         // Zufallswert aus den Prädikaten
-    let removedPraedikat: string = prädikate.splice(präZahl, 1);
-    //console.log(removedPrädikat + " ");
+        let removedPraedikat: string[] = praedikate.splice(zahlPra, 1);
+        //console.log(removedPrädikat + " ");
 
 
 
@@ -46,13 +44,12 @@ namespace RandomPoem {
 
 
         //creation of numbers: OBJEKT
-    let zahlObj: number = Math.floor(Math.random() * 6 );
-    let objZahl: number = zahlObj;
-    //console.log(objZahl);
+        let zahlObj: number = Math.floor(Math.random() * _objekte.length);
+        //console.log(objZahl);
 
         // Zufallswert aus den Objekten
-    let removedObject: string = objekte.splice(objZahl, 1);
-    //console.log(removedObject + " ");
+        let removedObject: string[] = objekte.splice(zahlObj, 1);
+        //console.log(removedObject + " ");
 
 
 
@@ -60,8 +57,8 @@ namespace RandomPoem {
 
 
 
-    let einVers: string = removedSubject[0], removedPraedikat[0], removedObject[0];
-    console.log(einVers);
-    return einVers;
+        let einVers: string = removedSubject[0] + " " + removedPraedikat[0] + " " + removedObject[0];
+        //console.log(einVers);
+        return einVers;
     }
-    }
+}
