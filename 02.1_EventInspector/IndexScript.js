831bfd717fdcf561e4e5abebd0ab1b104fd4e9c5
Ronja-Burger <ronja.burger@hs-furtwangen.de>
2022-03-29 21:25:25 +0200

    Aufgabe 3 fortgesetzt

---
+++ 02.1_EventInspector/IndexScript.js
@@ -0,0 +1,24 @@
+"use strict";
+var Greet;
+(function (Greet) {
+    window.addEventListener("load", hndLoad);
+    function hndLoad(_event) {
+        let input = document.querySelector("input");
+        if (!input)
+            return;
+        console.log(input);
+        input.addEventListener("change", hndChange);
+        document.addEventListener("greet", hndGreet);
+    }
+    function hndChange(_event) {
+        console.log(_event);
+        // test if value == "Hello"
+        let customEvent = new CustomEvent("greet", { bubbles: true });
+        _event.target?.dispatchEvent(customEvent);
+    }
+    function hndGreet(_event) {
+        let target = _event.target;
+        console.log("Greeted with", target.value);
+    }
+})(Greet || (Greet = {}));
+//# sourceMappingURL=IndexScript.js.map
