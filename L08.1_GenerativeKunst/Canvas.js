97699c99a21ea4933155d173a09cf095f92f0580
Ronja-Burger <ronja.burger@hs-furtwangen.de>
2022-05-05 20:09:25 +0200

    Canvas fertig

--- L08.1_GenerativeKunst/Canvas.js
+++ L08.1_GenerativeKunst/Canvas.js
@@ -6,21 +6,31 @@ var Canvas;
     function start(_event) {
         crc2 = document.querySelector("canvas")?.getContext("2d");
         triangle();
+        text();
+        circle();
+        square();
+        triangle();
         circle();
         square();
     }
     function triangle() {
         crc2.beginPath();
-        crc2.strokeStyle = "rgb (" + randomNumberRGB() + "," + randomNumberRGB() + "," + randomNumberRGB() + ")";
+        crc2.strokeStyle = "rgb(" + randomNumberRGB() + "," + randomNumberRGB() + "," + randomNumberRGB() + ")";
         crc2.moveTo(randomNumber(), 20);
         crc2.lineTo(randomNumber() + 40, 50);
         crc2.lineTo(randomNumber() - 10, 50);
         crc2.closePath();
         crc2.stroke();
     }
+    function text() {
+        crc2.beginPath();
+        crc2.font = randomNumberFontsize() + "px Arial";
+        crc2.fillText("Hello World", randomNumber(), randomNumber());
+        crc2.stroke();
+    }
     function circle() {
         crc2.beginPath();
-        crc2.strokeStyle = "rgb (" + randomNumberRGB() + "," + randomNumberRGB() + "," + randomNumberRGB() + ")";
+        crc2.strokeStyle = "rgb(" + randomNumberRGB() + "," + randomNumberRGB() + "," + randomNumberRGB() + ")";
         let path = new Path2D();
         path.arc(randomNumber(), randomNumber(), randomNumber(), 0, 2 * Math.PI);
         crc2.closePath();
@@ -28,18 +38,22 @@ var Canvas;
     }
     function square() {
         crc2.beginPath();
-        crc2.fillStyle = "rgb (" + randomNumberRGB() + "," + randomNumberRGB() + "," + randomNumberRGB() + ")";
+        crc2.fillStyle = "rgb(" + randomNumberRGB() + "," + randomNumberRGB() + "," + randomNumberRGB() + ")";
         crc2.fillRect(randomNumber(), randomNumber(), randomNumber(), randomNumber());
         crc2.closePath();
         crc2.stroke();
     }
     function randomNumber() {
-        let number = Math.floor(Math.random() * 200);
+        let number = Math.floor(Math.random() * 150);
         return number;
     }
     function randomNumberRGB() {
-        let RGBnumber = Math.floor(Math.random() * 256);
-        return RGBnumber;
+        let rgbNumber = Math.floor(Math.random() * 256);
+        return rgbNumber;
+    }
+    function randomNumberFontsize() {
+        let fontsizeNumber = Math.floor(Math.random() * 21);
+        return fontsizeNumber;
     }
 })(Canvas || (Canvas = {}));
 //# sourceMappingURL=Canvas.js.map
--- L08.1_GenerativeKunst/Canvas.ts
+++ L08.1_GenerativeKunst/Canvas.ts
@@ -9,11 +9,14 @@ namespace Canvas {
     text();
     circle();
     square();
+    triangle();
+    circle();
+    square();
   }
 
   function triangle(): void {
     crc2.beginPath();
-    crc2.strokeStyle = "rgb (" + randomNumberRGB() + "," + randomNumberRGB() + "," +  randomNumberRGB() + ")";
+    crc2.strokeStyle = "rgb(" + randomNumberRGB() + "," + randomNumberRGB() + "," +  randomNumberRGB() + ")";
     crc2.moveTo(randomNumber(), 20);
     crc2.lineTo(randomNumber() + 40, 50);
     crc2.lineTo(randomNumber() - 10, 50);
@@ -23,13 +26,14 @@ namespace Canvas {
 
   function text(): void {
     crc2.beginPath();
-    crc2.font = "30px Arial";
+    crc2.font = randomNumberFontsize() + "px Arial";
     crc2.fillText("Hello World", randomNumber(), randomNumber());
+    crc2.stroke();
   }
 
   function circle(): void {
     crc2.beginPath();
-    crc2.strokeStyle = "rgb (" + randomNumberRGB() + "," + randomNumberRGB() + "," +  randomNumberRGB() + ")";
+    crc2.strokeStyle = "rgb(" + randomNumberRGB() + "," + randomNumberRGB() + "," +  randomNumberRGB() + ")";
     let path: Path2D = new Path2D();
     path.arc(randomNumber(), randomNumber(), randomNumber(), 0, 2 * Math.PI);
     crc2.closePath();
@@ -38,19 +42,23 @@ namespace Canvas {
 
   function square(): void {
     crc2.beginPath();
-    crc2.fillStyle = "rgb (" + randomNumberRGB() + "," + randomNumberRGB() + "," +  randomNumberRGB() + ")";
+    crc2.fillStyle = "rgb(" + randomNumberRGB() + "," + randomNumberRGB() + "," +  randomNumberRGB() + ")";
     crc2.fillRect(randomNumber(), randomNumber(), randomNumber(), randomNumber());
     crc2.closePath();
     crc2.stroke();
   }
 
   function randomNumber(): number {
-    let number: number = Math.floor(Math.random() * 50);
+    let number: number = Math.floor(Math.random() * 150);
     return number;
   }
 
   function randomNumberRGB(): number {
     let rgbNumber: number = Math.floor(Math.random() * 256);
     return rgbNumber;
+  }
+  function randomNumberFontsize(): number {
+    let fontsizeNumber: number = Math.floor(Math.random() * 21);
+    return fontsizeNumber;
   }
 }
